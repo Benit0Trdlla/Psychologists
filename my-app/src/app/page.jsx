9307754f8fa1564996f84c4f929165831ffcbb36
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-04-23 11:21:06 -0300

    Advisors section: responsive. Cards Types of therapies.

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -704,31 +704,29 @@ export default function Home() {
 
         <div className="container text-center mt-5">
           <div className="row">
-            <div className="col">
+            <div className="col-12 col-md-6 col-lg-4">
               <img src="images/Asesores/1.svg" alt="" />
               <h4 className="fw-bold">Dificultades en las relaciones</h4>
               <p className="fw-normal fs-semibold">
                 Te cuesta entenderte con tu pareja, familiares, hijos, compañeros. Te sientes solo.
               </p>
             </div>
-            <div className="col">
+            <div className="col-12 col-md-6 col-lg-4">
               <img src="images/Asesores/2.svg" alt="" />
               <h4 className="fw-bold">Períodos difíciles en la vida</h4>
               <p className="fw-normal fs-semibold">
                 Estás atravesando una crisis personal debido a un divorcio, pérdida de empleo, cambio de residencia, etc. Buscas un propósito en la vida.
               </p>
             </div>
-            <div className="col">
+
+            <div className="col-12 col-md-6 col-lg-4">
               <img src="images/Asesores/3.svg" alt="" />
               <h4 className="fw-bold">Depresión y apatía</h4>
               <p className="fw-normal fs-semibold">
                 Sientes tristeza constante, pérdida de interés por la vida. No disfrutas lo que solías disfrutar.
               </p>
             </div>
-          </div>
-
-          <div className="row mt-2">
-            <div className="col">
+            <div className="col-12 col-md-6 col-lg-4">
               <img src="images/Asesores/4.svg" alt="" />
               <h4 className="fw-bold">Miedos, fobias, ansiedad
               </h4>
@@ -736,14 +734,15 @@ export default function Home() {
                 Te persigue una preocupación constante que limita tu vida. Tienes miedo de perder a alguien, tu trabajo, salud o a ti mismo.
               </p>
             </div>
-            <div className="col">
+
+            <div className="col-12 col-md-6 col-lg-4">
               <img src="images/Asesores/5.svg" alt="" />
               <h4 className="fw-bold">Relaciones contigo mismo</h4>
               <p className="fw-normal fs-semibold">
                 No estás seguro de tus habilidades, acciones, decisiones o apariencia. Quieres aprender a comprenderte y aceptarte, recuperar la sensibilidad corporal.
               </p>
             </div>
-            <div className="col">
+            <div className="col-12 col-md-6 col-lg-4">
               <img src="images/Asesores/6.svg" alt="" />
               <h4 className="fw-bold">Duelo, pérdidas, separaciones</h4>
               <p className="fw-normal fs-semibold">
@@ -755,6 +754,45 @@ export default function Home() {
           <p className="mb-0 p-0 mt-3 fw-medium">Esto es solo una parte. Cada solicitud es única.</p>
           <p className="fw-semibold">Por eso, nuestros especialistas ofrecen un enfoque individual que resuena con tu historia y necesidades.</p>
           <button className="btn btn-success p-3 mt-2 fw-semibold">Reservar una cita con un psicólogo</button>
+        </div>
+
+      </section>
+      {/* TIPOS DE TERAPIAS */}
+      <section className="container px-0 text-center py-5">
+        <h1>TEXT</h1>
+        <div className="row gap-3 mx-auto">
+          <div className="col-12 col-md-6 col-lg-4 border border-success rounded-4" style={{ height: "454.58px", width: "360px", padding: "20px" }}>
+            <div className="bg-success h-100 position-relative">
+              <div className="bg-white z-1 position-absolute top-50 start-50 translate-middle w-100 h-75">
+                <h1 className="fw-semibold mt-5">Terapia para todos</h1>
+                <p className="fw-normal fs-6 mt-3">
+                  Nuestros especialistas trabajan con diferentes clientes en diferentes etapas de sus vidas.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6 col-lg-4 border border-success rounded-4" style={{ height: "454.58px", width: "360px", padding: "20px" }}>
+            <div className="bg-success h-100 position-relative">
+              <div className="bg-white z-1 position-absolute top-50 start-50 translate-middle w-100 h-75">
+                <h1 className="fw-semibold mt-5">Terapia para todos</h1>
+                <p className="fw-normal fs-6 mt-3">
+                  Nuestros especialistas trabajan con diferentes clientes en diferentes etapas de sus vidas.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6 col-lg-4 border border-success rounded-4" style={{ height: "454.58px", width: "360px", padding: "20px" }}>
+            <div className="bg-success h-100 position-relative">
+              <div className="bg-white z-1 position-absolute top-50 start-50 translate-middle w-100 h-75">
+                <h1 className="fw-semibold mt-5">Terapia para todos</h1>
+                <p className="fw-medium mt-3">
+                  Nuestros especialistas trabajan con diferentes clientes en diferentes etapas de sus vidas.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
