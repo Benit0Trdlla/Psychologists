b256fadaed1f8cb9a0b3f4abe3c85b736f35d379
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-05-05 16:44:16 -0300

    Add: request an appointment section, not responsive yet.

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -10,13 +10,25 @@ export default function Home() {
 
   return (
     <>
-      <Navbar/>
-      <HeroSection/>
-      <CardsStats/>
-      <CardsPsychologists/>
-      <AdvisorsSection/>
-      <TypesTherapies/>
-      <PsychotherapyProcess/>
+      <Navbar />
+      <HeroSection />
+      <CardsStats />
+      <CardsPsychologists />
+      <AdvisorsSection />
+      <TypesTherapies />
+      <PsychotherapyProcess />
+      <section className="bg-secondary-subtle p-lg-5">
+        <div style={{ width: "1072px", height: "370px", position: "relative" }} className="border border-dark rounded mx-auto p-5 d-flex justify-content-between align-items-center">
+          <div>
+            <p className="fw-semibold fs-5">El camino hacia la armonía interior puede ser difícil, pero con el apoyo de nuestros psicólogos, nunca estará solo. 🤗</p>
+            <p className="fw-semibold fs-5">En Terappio están quienes están listos para ayudar.</p>
+            <button className="mt-3 fw-semibold btn btn-success py-3 px-4">Reservar una cita con un psicólogo</button>
+          </div>
+          <div>
+            <img src="Images/reservar-cita.svg" alt="" style={{position: "relative", right: "-100px"}}/>
+          </div>
+        </div>
+      </section>
 
     </>
   );
