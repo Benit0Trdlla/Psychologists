1d2d0c76cf9f328b1fb0ed748164a44724955d02
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-05-28 11:37:30 -0300

    Add filters

--- my-app/src/app/[country]/Psychologists/Search/page.jsx
+++ my-app/src/app/[country]/Psychologists/Search/page.jsx
@@ -3,8 +3,102 @@ export default async function PsychologistsSearch({ params }) {
 
     return (
         <>
-            <div className="container">
-                <h1>Encuentra tu psicólogo en {country}</h1>
+            <div className="container border border-success rounded-3 p-3">
+                <h1 className="text-center p-3 mb-0">Encuentra tu psicólogo en {country}</h1>
+
+                <div className="d-flex justify-content-center gap-4 mt-3">
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6" />
+                            </svg>
+                        </span>
+                        <input type="text" className="form-control" placeholder="Introduzca la ciudad aquí..." aria-label="City" aria-describedby="basic-addon1" />
+                    </div>
+
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill-gear" viewBox="0 0 16 16">
+                                <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4m9.886-3.54c.18-.613 1.048-.613 1.229 0l.043.148a.64.64 0 0 0 .921.382l.136-.074c.561-.306 1.175.308.87.869l-.075.136a.64.64 0 0 0 .382.92l.149.045c.612.18.612 1.048 0 1.229l-.15.043a.64.64 0 0 0-.38.921l.074.136c.305.561-.309 1.175-.87.87l-.136-.075a.64.64 0 0 0-.92.382l-.045.149c-.18.612-1.048.612-1.229 0l-.043-.15a.64.64 0 0 0-.921-.38l-.136.074c-.561.305-1.175-.309-.87-.87l.075-.136a.64.64 0 0 0-.382-.92l-.148-.045c-.613-.18-.613-1.048 0-1.229l.148-.043a.64.64 0 0 0 .382-.921l-.074-.136c-.306-.561.308-1.175.869-.87l.136.075a.64.64 0 0 0 .92-.382zM14 12.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0" />
+                            </svg>
+                        </span>
+                        <input type="text" className="form-control" placeholder="¿Para quién es su solicitud?" aria-label="City" aria-describedby="basic-addon2" />
+                    </div>
+
+                    <div className="input-group mb-3">
+                        <label className="input-group-text" htmlFor="inputGroupSelect01">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list-task" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M2 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5V3a.5.5 0 0 0-.5-.5zM3 3H2v1h1z" />
+                                <path d="M5 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M5.5 7a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 4a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1z" />
+                                <path fillRule="evenodd" d="M1.5 7a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5zM2 7h1v1H2zm0 3.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm1 .5H2v1h1z" />
+                            </svg>
+                        </label>
+                        <select className="form-select" id="inputGroupSelect01">
+                            <option defaultValue>Su solicitud</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className="d-flex justify-content-center gap-4">
+                    <div className="input-group mb-3">
+                        <label className="input-group-text" htmlFor="inputGroupSelect02">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-gender-ambiguous" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M11.5 1a.5.5 0 0 1 0-1h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-3.45 3.45A4 4 0 0 1 8.5 10.97V13H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V14H6a.5.5 0 0 1 0-1h1.5v-2.03a4 4 0 1 1 3.471-6.648L14.293 1zm-.997 4.346a3 3 0 1 0-5.006 3.309 3 3 0 0 0 5.006-3.31z" />
+                            </svg>
+                        </label>
+                        <select className="form-select" id="inputGroupSelect02">
+                            <option defaultValue>Género del psicoterapeuta</option>
+                            <option value="Mujer">Mujer</option>
+                            <option value="Hombre">Hombre</option>
+                        </select>
+                    </div>
+
+                    <div className="input-group mb-3">
+                        <select className="form-select" id="inputGroupSelect02">
+                            <option defaultValue>Método de terapia</option>
+                            <option value="Mujer">Mujer</option>
+                            <option value="Hombre">Hombre</option>
+                        </select>
+                    </div>
+
+                    <div className="input-group mb-3">
+                        <select className="form-select" id="inputGroupSelect02">
+                            <option defaultValue>Clasificar</option>
+                            <option value="Mujer">Mujer</option>
+                            <option value="Hombre">Hombre</option>
+                        </select>
+                    </div>
+
+                </div>
+
+                <div className="d-flex justify-content-center align-items-center gap-4">
+
+                    <div className="input-group mb-3 w-25">
+                        <select className="form-select" id="inputGroupSelect02">
+                            <option defaultValue>Clasificar</option>
+                            <option value="Mujer">Mujer</option>
+                            <option value="Hombre">Hombre</option>
+                        </select>
+                    </div>
+
+                    <div className="mb-lg-3 form-check">
+                        <input className="form-check-input" type="checkbox" value="" id="checkDefault" />
+                        <label className="form-check-label" htmlFor="checkDefault">
+                            Atención online
+                        </label>
+                    </div>
+
+                    <button className="mb-lg-3 btn btn-success">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="me-2 bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                        </svg>
+                        Buscar 
+                    </button>
+                </div>
+
             </div>
         </>
     );
