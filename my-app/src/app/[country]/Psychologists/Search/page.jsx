62d9fb31064112ba1cee537358cdd8a837c3fae7
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-07-24 10:40:32 -0300

    Update page.jsx

--- my-app/src/app/[country]/Psychologists/Search/page.jsx
+++ my-app/src/app/[country]/Psychologists/Search/page.jsx
@@ -1,11 +1,9 @@
-export default async function PsychologistsSearch({ params }) {
-    const { country } = await params
-
+export default function PsychologistsSearch() {
     return (
         <>
-            <div className="container border border-success rounded-3 p-3">
-                <h1 className="text-center p-3 mb-0">Encuentra tu psicólogo en {country}</h1>
-
+            <div className="container border border-success rounded-3">
+                <h1 className="text-center p-3 mb-0"> Buscar un psicólogo y psicoterapeuta en línea / en persona
+                </h1>
                 <div className="d-flex justify-content-center gap-4 mt-3">
                     <div className="input-group mb-3">
                         <span className="input-group-text" id="basic-addon1">
@@ -95,7 +93,7 @@ export default async function PsychologistsSearch({ params }) {
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="me-2 bi bi-search" viewBox="0 0 16 16">
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
                         </svg>
-                        Buscar 
+                        Buscar
                     </button>
                 </div>
 
